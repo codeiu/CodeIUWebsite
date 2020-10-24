df6b62b5693139d49666476fc900861cf0b623df
chmalex <chmalex@iu.edu>
2020-10-24 13:10:49 -0400

    backend1

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a303cd25b21ff329d8ec262b31d082c0'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template('home.html')
+	name = ""
+	if request.method == "POST":
+		name = " " + request.form['username']
+		app.logger.info(f'{name} is in')
+	else:
+		if request.args.get('username') is not None: name = " " + request.args.get('username')
+	return render_template('home.html', name=name)
 
 @app.route('/about')
 def about():
@@ -17,6 +25,20 @@ def calendar():
 @app.route('/videos')
 def videos():
 	return render_template('videos.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		app.logger.info('validated')
+		if request.method == 'POST':
+			app.logger.info('POST')
+			username = request.form['username']
+			return redirect(url_for('home'), code=307) 	# it took me way too long to figure out this damn code arg
+		elif request.method == 'GET':
+			app.logger.info('GET')
+			return redirect(url_for('home',name=form.username.data)) 
+	return render_template('register.html', form=form)
 
 
 if __name__ == "__main__":
