336af5c32813ea48aa15e90d470849eef57e6930
chmalex <chmalex@iu.edu>
2020-10-27 13:59:43 -0400

    backend2 - messy, but didn't break anything

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,4 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+from website import routes # has to go here to avoid circular import 
