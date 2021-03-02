636b172f844b06ff4bacc86b72dbf2f353ce268f
chmalex <chmalex@iu.edu>
2021-03-02 14:27:04 -0500

    getVideos promise

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ app.use(bodyparser.json());
 
 let youtube_template = "";
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
-
-/* Responds with a JSON of format { videos: [] } with Code@IU YouTube video links.
-At the moment, if there is an error, it will simply respond with an empty list*/
-app.get("/videos", (req, res) => {
+const getVideos = new Promise((resolve, reject) => {
   let url =
     "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8tF_u2E19Poo147OkY02zNwpNfFdVnYI&" +
     "type=video&part=snippet&channelId=UC1aXDYhzYCzOCNxJV6hYdLA&maxResults=10&q=";
@@ -29,18 +23,30 @@ app.get("/videos", (req, res) => {
         videoIds.push(item.id.videoId);
       }
     })
-    .catch((err) => alert(err))
+    .catch((err) => console.log("\n-- ERROR IN getVideos() --\n" + err))
     .finally(() => {
-      ejs.renderFile(
-        path.join(__dirname, "views", "videos.html"),
-        { videos: videoIds },
-        {},
-        (err, str) => {
-          res.send(str);
-        }
-      );
-      // res.sendFile(path.join(__dirname, "views", "videos.html"));
+      resolve(videoIds);
     });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+/* Responds with a JSON of format { videos: [] } with Code@IU YouTube video links.
+At the moment, if there is an error, it will simply respond with an empty list*/
+app.get("/videos", (req, res) => {
+  getVideos.then((videoIds, rej) => {
+    console.log(videoIds);
+    ejs.renderFile(
+      path.join(__dirname, "views", "videos.html"),
+      videoIds,
+      {},
+      (err, str) => {
+        res.send(str);
+      }
+    );
+  });
 });
 
 app.listen(port, () => {
