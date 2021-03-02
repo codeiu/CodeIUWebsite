0bc8194d940c8eb1598290802bd135eefda92cf5
chmalex <chmalex@iu.edu>
2021-03-02 17:17:42 -0500

    reform

--- index.js
+++ index.js
@@ -7,36 +7,33 @@ const express = require("express"),
 app = express();
 app.use(bodyparser.json());
 
-let youtube_template = "";
-
-const getVideos = new Promise((resolve, reject) => {
+const getVideos = () => {
   let url =
     "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8tF_u2E19Poo147OkY02zNwpNfFdVnYI&" +
     "type=video&part=snippet&channelId=UC1aXDYhzYCzOCNxJV6hYdLA&maxResults=10&q=";
   const youtube_url = "https://youtube.com/embed/";
-  let videoIds = [];
-  axios
+  return axios
     .get(url)
     .then((response) => {
+      let videoIds = [];
       for (let i in response.data.items) {
         let item = response.data.items[i];
         videoIds.push(item.id.videoId);
       }
+      return videoIds;
     })
-    .catch((err) => console.log("\n-- ERROR IN getVideos() --\n" + err))
-    .finally(() => {
-      resolve(videoIds);
+    .catch((err) => {
+      console.log("\n-- ERROR IN getVideos() --\n" + err);
+      return [];
     });
-});
+};
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
-/* Responds with a JSON of format { videos: [] } with Code@IU YouTube video links.
-At the moment, if there is an error, it will simply respond with an empty list*/
 app.get("/videos", (req, res) => {
-  getVideos.then((videoIds, rej) => {
+  getVideos().then((videoIds, rej) => {
     console.log(videoIds);
     ejs.renderFile(
       path.join(__dirname, "views", "videos.html"),
