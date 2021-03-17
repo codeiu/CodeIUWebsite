e1af695f89a3e723ba8216dbe70d34ab8c57a725
Zack Seliger <zseliger@gmail.com>
2021-03-16 20:03:02 -0400

    basic header and footer

--- index.js
+++ index.js
@@ -3,22 +3,14 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 app.get('/videos/:id', function(req, res) {
   res.send("hi");
 });
 
 app.get('/', function(req, res) {
-  res.render('videos', {
-    title: "hi mom",
-    videos: [
-      'hi',
-      'saef',
-      'asef',
-      '234',
-      '547',
-      'gr',
-    ]
-  });
+  res.render('homepage');
 });
 
 app.listen(process.env.PORT || 8080, function(err) {
