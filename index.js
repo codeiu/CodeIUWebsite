1d255d7fa262a77d04a7356144d66bde7cddf6ae
Zack Seliger <zseliger@gmail.com>
2021-03-30 19:57:19 -0400

    get videos from youtube

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get('/videos/:id', function(req, res) {
-  res.send("hi");
-});
+let obj = {
+	hi: "world",
+	num: 3,
+}
 
 app.get('/', function(req, res) {
   res.render('homepage');
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+
+app.get('/projects', function(req, res) {
+	res.render('projects');
+});
+
+app.get('/calendar', function(req, res) {
+	res.render('calendar');
+});
+
+app.get('/videos', function(req, res) {
+	fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8tF_u2E19Poo147OkY02zNwpNfFdVnYI&type=video&part=snippet&channelId=UC1aXDYhzYCzOCNxJV6hYdLA&maxResults=20&q=')
+	.then((videoStream) => videoStream.json())
+	.then((videos) => res.render('videos', { videos: videos.items }))
+	.catch((err) => console.warn(err));
 });
 
 app.listen(process.env.PORT || 8080, function(err) {
